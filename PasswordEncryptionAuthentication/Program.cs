04b95bc0c63fdd1d517d060ea5ae11c2c85b16c6
atmar <nathaniel.atmar@outlook.com>
2019-09-23 10:07:05 -0700

    stuff

--- PasswordEncryptionAuthentication/Program.cs
+++ PasswordEncryptionAuthentication/Program.cs
@@ -1,19 +1,13 @@
 ﻿using System;
+using Users;
 
 namespace PasswordEncryptionAuthentication
 {
-    public class UserList
-    {
-        public string _password;
-        public string _username;
-        
-    }
-
     class Program
     {   
-        
         public static void UserInterface()
         {
+            Console.Clear();
             Console.WriteLine("PASSWORD AUTHENTICATION SYSTEM\n");
             Console.WriteLine("Please select one option");
             Console.WriteLine("1. Establish an account");
@@ -24,25 +18,27 @@ namespace PasswordEncryptionAuthentication
             if (userInput == "1")
             {
                 //Do establish an account method
-                UserInterface();
+                
             }
             if (userInput == "2")
             {
                 //Do authenticate a user method
-                UserInterface();
             }
             if (userInput == "3")
             {
                 //Print all usernames, passwords, and encrypted passwords
                 Console.WriteLine("Any saved usernames or passwords will be lost");
                 Console.WriteLine("See you later alligator");
+                return;
                 // Wait some time then close application
             }
             if (userInput != "1" && userInput !="2" && userInput !="3")
             {
                 Console.WriteLine("\nOnly 1, 2, and 3 are correct inputs. Please select on of those numbers.\n");
-                UserInterface();
             }
+            
+            Console.ReadKey();
+            UserInterface();
         }
         static void Main(string[] args)
         {

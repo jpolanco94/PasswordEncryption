e5a8f8b92e10cc41d73bfe9f0f8cc092a56d85ee
atmar <nathaniel.atmar@outlook.com>
2019-09-24 13:10:56 -0700

    Added deleting from DB

--- PasswordEncryptionAuthentication/Program.cs
+++ PasswordEncryptionAuthentication/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Users;
 
 namespace PasswordEncryptionAuthentication
@@ -11,99 +12,99 @@ namespace PasswordEncryptionAuthentication
 
         public static void UserInterface()
         {
-            using (var db = new GroupAssignmentContext())
+            Console.Clear();
+            Console.WriteLine("-----------------------------------------------------------------\n");
+            Console.WriteLine("\t  PASSWORD AUTHENTICATION SYSTEM\n");
+            Console.WriteLine("\t  Please select one option");
+            Console.WriteLine("\t  1. Establish an account");
+            Console.WriteLine("\t  2. Authenticate a user");
+            Console.WriteLine("\t  3. Exit the system\n");
+            Console.Write("\t  Enter Selection:  ");
+            string userInput = Console.ReadLine();
+            Console.WriteLine("\n-----------------------------------------------------------------");
+
+            if (userInput == "1")
             {
-                Console.Clear();
-                Console.WriteLine("-----------------------------------------------------------------\n");
-                Console.WriteLine("\t  PASSWORD AUTHENTICATION SYSTEM\n");
-                Console.WriteLine("\t  Please select one option");
-                Console.WriteLine("\t  1. Establish an account");
-                Console.WriteLine("\t  2. Authenticate a user");
-                Console.WriteLine("\t  3. Exit the system\n");
-                Console.Write("\t  Enter Selection:  ");
-                string userInput = Console.ReadLine();
-                Console.WriteLine("\n-----------------------------------------------------------------");
+                Console.Write("Enter a desired user name: ");
+                string userName = Console.ReadLine();
 
-                if (userInput == "1")
+                if (!userList.Contains(userName))
                 {
-                    Console.Write("Enter a desired user name: ");
-                    string userName = Console.ReadLine();
-                    
-                    if (!userList.Contains(userName))
-                    {
-                        string userPassword = "";
-                        Console.Write("Enter a desired password: ");
-                        bool flag = true;
+                    string userPassword = "";
+                    Console.Write("Enter a desired password: ");
+                    bool flag = true;
 
-                        while (flag)
+                    while (flag)
+                    {
+                        char c = Console.ReadKey(true).KeyChar;
+                        if (c == '\r')
                         {
-                            char c = Console.ReadKey(true).KeyChar;
-                            if (c == '\r')
-                            {
-                                flag = false;
-                            }
-                            else
-                                userPassword += c.ToString();
+                            flag = false;
                         }
-                        userList.Add(userName);
-                        u[userName] = UserList.Encrypt(userPassword);
+                        else
+                            userPassword += c.ToString();
+                    }
+                    userList.Add(userName);
+                    u[userName] = UserList.Encrypt(userPassword);
+                    using (var db = new GroupAssignmentContext())
+                    {
                         db.Users.Add(new Users { UserName = userName, Password = u[userName] });
                         var count = db.SaveChanges();
                     }
-                    else
-                    {
-                        Console.WriteLine("This username is unavailable");
-                    }
                 }
-                if (userInput == "2")
+                else
                 {
-                    Console.Write("Enter your user name: ");
-                    string userName = Console.ReadLine();
-                    Console.Write("Enter the password: ");
-                    string userPassword = Console.ReadLine();
-
-                    if (UserList.Encrypt(userPassword) == u[userName])
-                    {
-                        Console.WriteLine($"User {userName} has been authenticated");
-                        Console.WriteLine($"  User name = {userName}");
-                        Console.WriteLine($"  Password = {userPassword}");
-                        Console.WriteLine($"  Encrypted Password = {u[userName]}");
-                    }
-                    else
-                        Console.WriteLine("Username / Password invalid.");
+                    Console.WriteLine("This username is unavailable");
                 }
-                if (userInput == "3")
-                {
-                    Console.WriteLine("\nHere is a list of all the users");
-                    for (int i = 0; i < userList.Count; i++)
-                    {
-                        Console.WriteLine($"  [{userList[i]}, {u[userList[i]]}]");
-                    }
-                    Console.WriteLine("\nErasing all user accounts.");
-                    Console.WriteLine("See you later alligator");
-                    return;
-                    // Wait some time then close application
-                }
-                if (userInput == "Access_Admin_Controls")
-                {
-                    u["Admin"] = UserList.Encrypt("qwerty123456");
-                    Console.Write("Enter the password: ");
-                    string checkPassword = Console.ReadLine();
-
-                    if (UserList.Encrypt(checkPassword) == u["Admin"])
-                    {
-                        Console.WriteLine("Access Granted.");
-                        AdminInterface();
-                    }
-                }
-                if (userInput != "1" && userInput != "2" && userInput != "3" && userInput != "Access_Admin_Controls")
-                {
-                    Console.WriteLine("\nOnly 1, 2, and 3 are correct inputs. Please select one of those numbers.\n");
-                }
-                Console.WriteLine("\nPress any key to continue.");
-                Console.ReadKey();
-                UserInterface();
             }
+            if (userInput == "2")
+            {
+                Console.Write("Enter your user name: ");
+                string userName = Console.ReadLine();
+                Console.Write("Enter the password: ");
+                string userPassword = Console.ReadLine();
+
+                if (UserList.Encrypt(userPassword) == u[userName])
+                {
+                    Console.WriteLine($"User {userName} has been authenticated");
+                    Console.WriteLine($"  User name = {userName}");
+                    Console.WriteLine($"  Password = {userPassword}");
+                    Console.WriteLine($"  Encrypted Password = {u[userName]}");
+                }
+                else
+                    Console.WriteLine("Username / Password invalid.");
+            }
+            if (userInput == "3")
+            {
+                Console.WriteLine("\nHere is a list of all the users");
+                for (int i = 0; i < userList.Count; i++)
+                {
+                    Console.WriteLine($"  [{userList[i]}, {u[userList[i]]}]");
+                }
+                Console.WriteLine("\nErasing all user accounts.");
+                Console.WriteLine("See you later alligator");
+                return;
+                // Wait some time then close application
+            }
+            if (userInput == "Access_Admin_Controls")
+            {
+                u["Admin"] = UserList.Encrypt("qwerty123456");
+                Console.Write("Enter the password: ");
+                string checkPassword = Console.ReadLine();
+
+                if (UserList.Encrypt(checkPassword) == u["Admin"])
+                {
+                    Console.WriteLine("Access Granted.");
+                    AdminInterface();
+                }
+            }
+            if (userInput != "1" && userInput != "2" && userInput != "3" && userInput != "Access_Admin_Controls")
+            {
+                Console.WriteLine("\nOnly 1, 2, and 3 are correct inputs. Please select one of those numbers.\n");
+            }
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadKey();
+            UserInterface();
         }
         public static void AdminInterface()
         {
@@ -127,12 +128,14 @@ namespace PasswordEncryptionAuthentication
                     Console.WriteLine($"  [{userList[i]}, {u[userList[i]]}]");
                 }
             }
+
             if (userInput == "2")
             {
                 //Only uses while running
                 Console.Write("Enter new password: ");
                 u["Admin"] = Console.ReadLine();
             }
+
             if (userInput == "3")
             {
                 for (int i = 0; i < userList.Count; i++)
@@ -143,12 +146,20 @@ namespace PasswordEncryptionAuthentication
                 string userName = Console.ReadLine();
                 u.DeleteUser(userName);
                 userList.Remove(userName);
+
+                using (var db = new GroupAssignmentContext())
+                {
+                    db.Users.RemoveRange(db.Users.Where(x => x.UserName == userName));
+                    db.SaveChanges();
+                }
             }
+
             if (userInput == "4")
             {
                 Console.WriteLine("Bye bye!");
                 return;
             }
+
             if (userInput != "1" && userInput != "2" && userInput != "3" && userInput != "4")
             {
                 Console.WriteLine("\nOnly 1, 2, 3, and 4 are correct inputs. Please select one of those numbers.\n");
@@ -158,13 +169,7 @@ namespace PasswordEncryptionAuthentication
             Console.ReadKey();
             AdminInterface();
         }
-        //opens a file, reads stuff in it to -- static List<string> userList = new List<string>();
-        void OpenFile()
-        { }
 
-        //closes a file, writes stuff in static List<string> userList = new List<string>(); to the file
-        void CloseFile()
-        { }
         static void Main(string[] args)
         {
 
